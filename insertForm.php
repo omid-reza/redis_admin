6bcb3e56ab37565e76ba5306ff6eb9f2c41c83d4
omi reza <snip77.orh@gmail.com>
2018-09-02 06:09:20 -0800

    add insertForm.css for insertForm.php

--- insertForm.php
+++ insertForm.php
@@ -2,20 +2,18 @@
 <html>
 <head>
     <title>insert - redis console</title>
-    <style type="text/css">
-        body{ background-image: linear-gradient(to left,#FC5252,#F237FE) }
-    </style>
     <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/insertForm.css">
 </head>
 <body>
-    <div class="container" style="margin-top: 20%" id="app">
+    <div class="container PartSeven" id="app">
       <div class=" container card text-white bg-dark mb-3" >
         <form method="post" action="../insert.php">
           <div class="card-header">
             Insert
-            <a style="float: right" href="../" class="btn btn-warning">Main Page</a>
+            <a href="../" class="btn btn-warning PartOne">Main Page</a>
           </div>
-          <div class="form-group" style="margin-top: 1%">
+          <div class="form-group PartTwo">
             <label for="exampleFormControlSelect1">Type</label>
             <select name="type" class="form-control" id="Type" v-model="type" @change="typeChange()">
               <option>String</option>
@@ -24,27 +22,27 @@
               <option>Set</option>
             </select>
           </div>
-          <div class="form-group row" style="margin-top: 2%" v-if="type != '' && addedExpire == true">
+          <div class="form-group row PartThree" v-if="type != '' && addedExpire == true">
             <label for="key" class="col-sm-2 col-form-label">Expire in (second)</label>
             <div class="col-sm-10">
               <input v-model="expireIn" type="number"  width="100%" class="form-control" name="expire">
             </div>
           </div>
-          <div class="form-group row" style="margin-top: 2%" v-if="type != ''">
+          <div class="form-group row PartThree" v-if="type != ''">
             <label for="key" class="col-sm-2 col-form-label">Key</label>
             <div class="col-sm-10">
               <input v-model="key" type="text"  width="100%" class="form-control" name="key">
             </div>
           </div>
-            <div class="form-group row" style="margin-top: 2%" v-if="type != ''" v-for="index in valuecount">
+            <div class="form-group row PartThree" v-if="type != ''" v-for="index in valuecount">
             <label for="key" class="col-sm-2 col-form-label">value</label>
             <div class="col-sm-10">
               <input type="text"  width="100%" class="form-control" name="value[]">
             </div>
           </div>
-            <button v-if="type!=''" type="submit" style="float: right;margin-bottom: 2%" class="btn btn-success">Insert</button> 
-            <a v-if="type != ''" v-if="type!='String'" @click="addValue()" style="float: right;margin-right: 1%;color: gray" class="btn btn-light">Add</a>
-            <a v-if="type != ''" @click="addExpire()" style="float: right;margin-right: 1%" class="btn btn-warning">
+            <button v-if="type!=''" type="submit" class="btn btn-success PartFour">Insert</button> 
+            <a v-if="type != ''" v-if="type!='String'" @click="addValue()" class="btn btn-light PartFive" style="color: gray;">Add</a>
+            <a v-if="type != ''" @click="addExpire()" class="btn btn-warning PartSix">
               {{ expireKeyText }}
             </a>
         </form>
