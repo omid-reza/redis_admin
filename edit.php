d98f737f161fb1ab7f28d3fd9ca0bba061d8971b
omi reza <snip77.orh@gmail.com>
2018-09-02 17:30:10 -0800

    add edit.css to edit.php

--- edit.php
+++ edit.php
@@ -2,9 +2,7 @@
 <html>
 <head>
 	<title>edit - redis console</title>
-    <style type="text/css">
-        body{ background-image: linear-gradient(to left,#FC5252,#F237FE) }
-    </style>
+    <link rel="stylesheet" type="text/css" href="assets/css/edit.css">
     <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
 </head>
 <body>
@@ -13,32 +11,32 @@
         $config=new Config();
         $client=$config->connect();
         if (is_string($client)) { ?>
-            <div class="container jumbotron" style="margin-top: 100px">
-                <span style="height: 50px;width: 150px;font-size: 20px; margin-bottom:10px " class="btn btn-outline-danger">errors</span>
+            <div class="container jumbotron PartTwo">
+                <span class="btn btn-outline-danger PartOne">errors</span>
                 <div class="alert alert-danger" role="alert">
                   <?php echo $client ?>
                 </div>
             </div>
     <?php }else if( ! isset($_GET['pervios_key'])){ ?>
-            <div class="container jumbotron" style="margin-top: 100px">
-                <span style="height: 50px;width: 150px;font-size: 20px; margin-bottom:10px " class="btn btn-outline-danger">errors</span>
-                <a style="float: right;" href="../" class="btn btn-warning">Back</a>
+            <div class="container jumbotron PartTwo">
+                <span class="btn btn-outline-danger PartOne">errors</span>
+                <a href="../" class="btn btn-warning PartThree">Back</a>
                 <div class="alert alert-danger" role="alert">
                   please set pervios_key in header ! ! !
                 </div>
             </div>
         <?php }else if( ! isset($_GET['value'])){ ?>
-            <div class="container jumbotron" style="margin-top: 100px">
-                <span style="height: 50px;width: 150px;font-size: 20px; margin-bottom:10px " class="btn btn-outline-danger">errors</span>
-                <a style="float: right;" href="../" class="btn btn-warning">Back</a>
+            <div class="container jumbotron PartTwo">
+                <span class="btn btn-outline-danger PartOne">errors</span>
+                <a href="../" class="btn btn-warning PartThree">Back</a>
                 <div class="alert alert-danger" role="alert">
                   please set value in header ! ! !
                 </div>
             </div>
 		<?php }else if(is_null($client->get($_GET['pervios_key']))){ ?>
-			<div class="container jumbotron" style="margin-top: 100px">
-                <span style="height: 50px;width: 150px;font-size: 20px; margin-bottom:10px " class="btn btn-outline-danger">errors</span>
-                <a style="float: right;" href="../" class="btn btn-warning">Back</a>
+			<div class="container jumbotron PartTwo">
+                <span class="btn btn-outline-danger PartOne">errors</span>
+                <a href="../" class="btn btn-warning PartThree">Back</a>
                 <div class="alert alert-danger" role="alert">
                   No record with key <?php echo $_GET['pervios_key']; ?>
                 </div>
@@ -51,12 +49,12 @@
                 }
                 $client->set($_GET['key'], $_GET['value']);
             ?>
-            <div class="container" style="margin-top: 100px">
+            <div class="container PartTwo">
                 <div class="card bg-light mb-3" >
                   <div class="card-header">
                     Updated
-                  <a style="float: right;" href="../" class="btn btn-warning">Main Page</a>
-                  <a style="float: right;margin-right: 3%" href="../show.php?key=<?php echo $_GET['key']?>" class="btn btn-light">View Record</a>
+                  <a href="../" class="btn btn-warning PartThree">Main Page</a>
+                  <a href="../show.php?key=<?php echo $_GET['key']?>" class="btn btn-light PartFour">View Record</a>
                   </div>
                   <div class="card-body">
                     <h5 class="card-title">record with key <span style="color: red"><?php echo $_GET['pervios_key']; ?></span> updated</h5>
