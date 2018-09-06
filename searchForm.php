0066fd8926cf2a66ed965c9bf18108ccdf5f2e61
omi reza <snip77.orh@gmail.com>
2018-09-06 02:29:25 -0800

    add searchForm.css to searchForm.php

--- searchForm.php
+++ searchForm.php
@@ -2,26 +2,24 @@
 <html>
 <head>
     <title>search - redis console</title>
-    <style type="text/css">
-        body{ background-image: linear-gradient(to left,#FC5252,#F237FE) }
-    </style>
     <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/searchForm.css">
 </head>
 <body>
-    <div class="container" style="margin-top: 20%">
+    <div class="container partTwo">
       <div class=" container card text-white bg-dark mb-3" >
         <form action="../searchResualt.php" method="get">
           <div class="card-header">
             Search
-            <a style="float: right" href="../" class="btn btn-warning">Main Page</a>
+            <a href="../" class="btn btn-warning partOne">Main Page</a>
           </div>
-          <div class="form-group row" style="margin-top: 2%">
+          <div class="form-group row partThree">
             <label for="key" class="col-sm-2 col-form-label">Key to search</label>
             <div class="col-sm-10">
               <input type="text" required width="100%" class="form-control" name="key">
             </div>
             </div>
-              <button type="submit" style="margin-bottom: 1%;float: right;" class="btn btn-primary">Search</button>
+              <button type="submit" class="btn btn-primary partOne PartFour">Search</button>
         </form>
       </div>
     </div>
