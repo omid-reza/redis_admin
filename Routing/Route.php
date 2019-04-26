f0ade872987fc1fdb353d76af0a15edfd79e62f0
omid reza <snip77.orh@gmail.com>
2019-04-26 19:22:12 +0000

    Apply fixes from StyleCI

--- Routing/Route.php
+++ Routing/Route.php
@@ -2,23 +2,21 @@
 
 class Route
 {
-	private $request;
+    private $request;
 
-	public function __construct()
-	{
-		$request='/';
-		if (isset($_SERVER['PATH_INFO'])) {
-			$request=$_SERVER['PATH_INFO'];
-		}
-		$this->request=$request;
-	}
+    public function __construct()
+    {
+        $request = '/';
+        if (isset($_SERVER['PATH_INFO'])) {
+            $request = $_SERVER['PATH_INFO'];
+        }
+        $this->request = $request;
+    }
 
-	public function get($url, $path)
-	{
-		if ($this->request==$url)
-		{
-			require_once __DIR__.'/../'.$path.'.php';	
-		}
-	}
-
+    public function get($url, $path)
+    {
+        if ($this->request == $url) {
+            require_once __DIR__.'/../'.$path.'.php';
+        }
+    }
 }
